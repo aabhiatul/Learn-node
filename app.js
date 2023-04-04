a8e62ae9f93da9f2821cbd6a34d75612b2ea566c
Aabhi_atul <aabhi_atul@Aabhi-atuls-MacBook-Air.local>
2023-04-04 23:56:53 +0530

    crud operatio on file system

--- app.js
+++ app.js
@@ -39,9 +39,36 @@ console.log("hello wordwesfsld!")
 
 // ----Display List of files ----
 
+// const fs = require('fs')
+// const path = require('path')
+// const dirPath = path.join(__dirname,'files')
+// // for(i=0;i<5;i++){
+// //     fs.writeFileSync(`${dirPath}/hello${i}.txt`,'simple file')
+// // }
+// fs.readdir(dirPath,(err,files) => {
+//     files.map(item => {
+//         console.log('this file name is - '+item)
+//     })
+// })
+
+// ----crud operation with file system ----
+
 const fs = require('fs')
 const path = require('path')
-const dirPath = path.join(__dirname,'files')
-for(i=0;i<5;i++){
-    fs.writeFileSync(`${dirPath}/hello${i}.txt`,'simple file')
-}
+const dirPath = path.join(__dirname,'crud')
+const filepath = `${dirPath}/apple.text`
+
+// fs.writeFileSync(filepath,"this is a file.") - write
+// fs.readFile(filepath,'utf-8',(err,item)=>{
+//     console.log(item)
+// }) - read
+// fs.appendFile(filepath," for new requirement!",(err) => {
+//     if(err) return
+//     console.log("updated")
+// }) - update
+// fs.rename(filepath,`${dirPath}/fruit.txt`,(err) =>{
+//     if(err) return
+//     console.log("update!")
+// }) - rename
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`) - delete
